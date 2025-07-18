2f57401beee7cb4a57c272637284ed49fd7a8671
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-18 17:08:04 +0200

    feat: something wrong

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // Attraverso l'apposita API di Boolean
 // https://flynn.boolean.careers/exercises/api/random/mail
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-
+console.log('Script JS caricato');
+console.log('vedi')
 
 //definizione di variabili 
-const lista = document.getElementById('lista')
+// const lista = document.getElementById('lista')
 
 //adesso con il for ciclo la chiamata all'API 10 volte in modo da avere poi un li composta da 10 email
 
 
-const mailApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
+// const mailApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-const maiList = axios.get(mailApi).then((resp) => {})
+// const maiList = axios.get(mailApi).then((resp) => {
+  
+  
+// })
