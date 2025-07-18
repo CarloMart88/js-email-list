477438e18dcedec525530cc0e2893bd1743753d6
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-18 16:53:23 +0200

    feat: creating variables and then start the call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+
 //definizione di variabili 
 const lista = document.getElementById('lista')
+
+//adesso con il for ciclo la chiamata all'API 10 volte in modo da avere poi un li composta da 10 email
+
+
+const mailApi = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+const maiList = axios.get(mailApi).then((resp) => {})
